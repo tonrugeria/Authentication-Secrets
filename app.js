7e898d2f62bc2ee43356153f275bbd71d2a41f52
TON RUGERIA <tonrugeria@gmail.com>
2022-06-21 18:13:34 +0800

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,10 +13,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema = {
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -31,7 +37,7 @@ app.get("/register", (req, res) => {
   res.render("register")
 })
 
-app.post("/", (req, res) => {
+app.post("/register", (req, res) => {
   const newUser = new User ({
     email: req.body.username,
     password: req.body.password
